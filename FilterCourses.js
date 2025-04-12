1e65c796f70554cf322399099f9103cd3ac9c69a
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2025-04-12 13:47:42 -0500

    More work on filtering

--- FilterCourses.js
+++ FilterCourses.js
@@ -1,72 +1,82 @@
-const response = await fetch("./Assets/Data/Data.json");
+async function getCourses(){
+    const response = await fetch("./Assets/Data/Data.json");
 
-let data = await response.json();
+    let data = await response.json();
 
-let courses = data.Courses;
+    let courses = data.Courses;
 
-console.log(courses);
+    const activeCourses = document.createElement("div");
+    activeCourses.id = "activeCourses";
 
-const activeCourses = document.createElement("div");
-activeCourses.id = "activeCourses";
+    const courseBody = document.getElementById("courses");
+    courseBody.appendChild(activeCourses);
 
-const courseBody = document.getElementById("courses");
-courseBody.appendChild(activeCourses);
+    for(let i = 0; i < courses.length; i++){
 
-for(let i = 0; i < courses.length; i++){
-    const courseInfo = document.createElement("div");
-    courseInfo.className = "courseCard";
+        const courseInfo = document.createElement("div");
+        courseInfo.className = "courseCard";
 
-    const courseCardTop = document.createElement("div");
-    courseCardTop.className = "courseCardTop";
+        const courseCardTop = document.createElement("div");
+        courseCardTop.className = "courseCardTop";
 
-    const courseName = document.createElement("p");
-    courseName.className = "courseTitle";
-    courseName.innerHTML = courses[i].courseName;
+        const courseName = document.createElement("p");
+        courseName.className = "courseTitle";
+        courseName.innerHTML = courses[i].courseName;
 
-    const addButton = document.createElement("button");
-    addButton.className = "addToPlan";
-    addButton.innerHTML = "Add to Plan";
+        const addButton = document.createElement("button");
+        addButton.className = "addToPlan";
+        addButton.innerHTML = "Add to Plan";
 
-    courseCardTop.appendChild(courseName);
-    courseCardTop.appendChild(addButton);
+        courseCardTop.appendChild(courseName);
+        courseCardTop.appendChild(addButton);
 
-    const courseRating = document.createElement("p");
-    courseRating.className="courseRating";
-    courseRating.innerHTML = "Rating: ".concat(courses[i].courseRating);
+        const courseRating = document.createElement("p");
+        courseRating.className="courseRating";
+        courseRating.innerHTML = "Rating: ".concat(courses[i].courseRating);
 
-    const skills = document.createElement("p");
-    let skillString = "Skills: ";
-    for(let j = 0; j < courses[i].skills.length; j++){
-        skillString = skillString.concat(courses[i].skills[j], " ");
+        const skills = document.createElement("p");
+        let skillString = "Skills: ";
+        for(let j = 0; j < courses[i].skills.length; j++){
+            skillString = skillString.concat(courses[i].skills[j], " ");
+        }
+        skills.className="courseSkills";
+        skills.innerHTML = skillString;
+
+
+        let dayString = "Days and Times: ";
+        for(let j = 0; j < courses[i].Days.length; j++){
+            dayString = dayString.concat(courses[i].Days[j], ", ");
+        }
+
+        const times = document.createElement("p");
+        dayString = dayString.concat(courses[i].Times[0], "-", courses[i].Times[1]);
+        times.className="courseTimes";
+        times.innerHTML = dayString;
+
+        const Tags = document.createElement("p");
+        let tagString = "Tags: ";
+        for(let j = 0; j < courses[i].Tags.length; j++){
+            tagString = tagString.concat(courses[i].Tags[j], " ");
+        }
+
+        Tags.innerHTML = tagString;
+        Tags.className = "courseTags";
+
+        courseInfo.appendChild(courseCardTop);
+        courseInfo.appendChild(courseRating);
+        courseInfo.appendChild(skills);
+        courseInfo.appendChild(times);
+        courseInfo.appendChild(Tags);
+
+        courseInfo.id = courses[i].courseID;
+        courseIDs.push(courses[i].courseID);
+
+        activeCourses.appendChild(courseInfo);
     }
-    skills.className="courseSkills";
-    skills.innerHTML = skillString;
-
-
-    let dayString = "Days and Times: ";
-    for(let j = 0; j < courses[i].Days.length; j++){
-        dayString = dayString.concat(courses[i].Days[j], ", ");
-    }
-
-    const times = document.createElement("p");
-    dayString = dayString.concat(courses[i].Times[0], "-", courses[i].Times[1]);
-    times.className="courseTimes";
-    times.innerHTML = dayString;
-
-    const Tags = document.createElement("p");
-    let tagString = "Tags: ";
-    for(let j = 0; j < courses[i].Tags.length; j++){
-        tagString = tagString.concat(courses[i].Tags[j], " ");
-    }
-
-    Tags.innerHTML = tagString;
-    Tags.className = "courseTags";
-
-    courseInfo.appendChild(courseCardTop);
-    courseInfo.appendChild(courseRating);
-    courseInfo.appendChild(skills);
-    courseInfo.appendChild(times);
-    courseInfo.appendChild(Tags);
-
-    activeCourses.appendChild(courseInfo);
 }
+
+let courseIDs = [];
+
+getCourses();
+
+console.log(courseIDs);
