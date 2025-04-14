9a8c073a8a6ae1f4768171103c60dc78080c80b2
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2025-04-14 17:14:37 -0500

    Internship and Club starting point

---
+++ FilterClubs.js
@@ -0,0 +1,133 @@
+// this function extracts and builds our clubs from our Data.json file
+function getclubs(){
+
+    // activeclubs seperates our filter from the club cards
+    const activeclubs = document.createElement("div");
+    activeclubs.id = "activeclubs";
+
+    const clubBody = document.getElementById("clubs");
+    clubBody.appendChild(activeclubs);
+
+    // for every entry from our json file...
+    for(let i = 0; i < clubs.length; i++){
+
+        // get name, rating, skills, tags, etc...
+        const clubInfo = document.createElement("div");
+        clubInfo.className = "clubCard";
+
+        const clubName = document.createElement("p");
+        clubName.className = "clubCardName";
+        clubName.innerHTML = clubs[i].clubName;
+
+        const clubDesc = document.createElement("p");
+        clubDesc.className = "clubCardDesc";
+        clubDesc.innerHTML = clubs[i].clubDesc;
+
+
+        // put it all together in a neat little card
+        clubInfo.appendChild(clubName);
+        clubInfo.appendChild(clubDesc);
+
+        clubInfo.id = clubs[i].clubID;
+        clubIDs.push(clubs[i].clubID);
+
+        activeclubs.appendChild(clubInfo);
+    }
+}
+
+// load up our json file
+const response = await fetch("./Assets/Data/Data.json");
+
+let data = await response.json();
+
+let clubs = data.Clubs;
+
+let clubIDs = [];
+
+getclubs();
+
+// just for debug purposes
+console.log(clubIDs);
+
+// now let's look at the filter functions. The Filter should do something every time it is ever touched
+const filter = document.getElementById("classFilter");
+filter.addEventListener('input', function(event){
+    console.log("Something happened with filter!");
+
+    // start by getting search text if inputted
+    let filterText = document.getElementsByClassName("filterSearch")[0].value;
+    console.log(filterText);
+
+    // then we get every checkbox that is active
+    let activeChecks = filter.querySelectorAll('input[type="checkbox"]:checked');
+    console.log(activeChecks);
+
+    // from the checkboxes, we want the id of them, this is important
+    let filterVals = [];
+    for(let i = 0; i < activeChecks.length; i++){
+        filterVals.push(activeChecks[i].id);
+    }
+
+    console.log(filterVals);
+
+    // now what we want to do is focus on two cases: there is search data, there is not
+    let validIds = [];
+    for(let i = 0; i < clubs.length; i++){
+
+        // if we have search data, identify if the data appears in club names
+        if(filterText.length > 0){
+            console.log(clubs[i].clubName.toLowerCase());
+            if(clubs[i].clubName.toLowerCase().includes(filterText)){
+                // if so, we must now check if there are any check filters, if not then we can add the id to valid ids
+                if(filterVals.length == 0){
+                    validIds.push(clubs[i].clubID);
+                }
+                // otherwise, see if one of the filter values correlates with one of the aspects of our club
+                else{
+                    for(let j = 0; j < filterVals.length; j++){
+                        if(clubs[i].Industry == filterVals[j] || clubs[i].School == filterVals[j] || clubs[i].Major == filterVals[j]){
+                            validIds.push(clubs[i].clubID);
+                        }
+                    }
+                }
+            }
+        // no filtertext? just check for filter values with checkboxes like above then
+        }else{
+            if(filterVals.length == 0){
+                validIds.push(clubs[i].clubID);
+            }
+            else{
+                for(let j = 0; j < filterVals.length; j++){
+                    if(clubs[i].Industry == filterVals[j] || clubs[i].School == filterVals[j] || clubs[i].Major == filterVals[j]){
+                        validIds.push(clubs[i].clubID);
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(validIds);
+
+    // now that we finally have a set of valid ids, we can hide those that shouldn't be seen
+    const clubCards = document.getElementsByClassName("clubCard");
+    console.log(clubCards);
+
+    // go through each club, if it's valid, show it, otherwise display nothing
+    for(let i = 0; i < clubCards.length; i++){
+        let toBeHidden = true;
+        for(let j = 0; j < validIds.length; j++){
+            if(validIds[j] == clubCards[i].id){
+
+                console.log("Element won't be hidden");
+                toBeHidden = false;
+                break;
+            }
+        }
+        if(toBeHidden == true){
+            clubCards[i].style.display = "none";
+        }
+        else{
+            clubCards[i].style.display = "";
+        }
+    }
+});
---
+++ FilterInternships.js
@@ -0,0 +1,134 @@
+// this function extracts and builds our internships from our Data.json file
+function getinternships(){
+
+    // activeinternships seperates our filter from the internship cards
+    const activeinternships = document.createElement("div");
+    activeinternships.id = "activeinternships";
+
+    const internshipBody = document.getElementById("internships");
+    internshipBody.appendChild(activeinternships);
+
+    // for every entry from our json file...
+    for(let i = 0; i < internships.length; i++){
+
+        // get name, rating, skills, tags, etc...
+        const internshipInfo = document.createElement("div");
+        internshipInfo.className = "internshipCard";
+
+        const internshipName = document.createElement("p");
+        internshipName.className = "internshipCardName";
+        internshipName.innerHTML = internships[i].internshipName;
+
+        const internshipDesc = document.createElement("p");
+        internshipDesc.className = "internshipCardDesc";
+        internshipDesc.innerHTML = internships[i].internshipDesc;
+
+
+        // put it all together in a neat little card
+        internshipInfo.appendChild(internshipName);
+        internshipInfo.appendChild(internshipDesc);
+
+
+        internshipInfo.id = internships[i].internshipID;
+        internshipIDs.push(internships[i].internshipID);
+
+        activeinternships.appendChild(internshipInfo);
+    }
+}
+
+// load up our json file
+const response = await fetch("./Assets/Data/Data.json");
+
+let data = await response.json();
+
+let internships = data.Internships;
+
+let internshipIDs = [];
+
+getinternships();
+
+// just for debug purposes
+console.log(internshipIDs);
+
+// now let's look at the filter functions. The Filter should do something every time it is ever touched
+const filter = document.getElementById("classFilter");
+filter.addEventListener('input', function(event){
+    console.log("Something happened with filter!");
+
+    // start by getting search text if inputted
+    let filterText = document.getElementsByClassName("filterSearch")[0].value;
+    console.log(filterText);
+
+    // then we get every checkbox that is active
+    let activeChecks = filter.querySelectorAll('input[type="checkbox"]:checked');
+    console.log(activeChecks);
+
+    // from the checkboxes, we want the id of them, this is important
+    let filterVals = [];
+    for(let i = 0; i < activeChecks.length; i++){
+        filterVals.push(activeChecks[i].id);
+    }
+
+    console.log(filterVals);
+
+    // now what we want to do is focus on two cases: there is search data, there is not
+    let validIds = [];
+    for(let i = 0; i < internships.length; i++){
+
+        // if we have search data, identify if the data appears in internship names
+        if(filterText.length > 0){
+            console.log(internships[i].internshipName.toLowerCase());
+            if(internships[i].internshipName.toLowerCase().includes(filterText)){
+                // if so, we must now check if there are any check filters, if not then we can add the id to valid ids
+                if(filterVals.length == 0){
+                    validIds.push(internships[i].internshipID);
+                }
+                // otherwise, see if one of the filter values correlates with one of the aspects of our internship
+                else{
+                    for(let j = 0; j < filterVals.length; j++){
+                        if(internships[i].Industry == filterVals[j] || internships[i].School == filterVals[j] || internships[i].Major == filterVals[j]){
+                            validIds.push(internships[i].internshipID);
+                        }
+                    }
+                }
+            }
+        // no filtertext? just check for filter values with checkboxes like above then
+        }else{
+            if(filterVals.length == 0){
+                validIds.push(internships[i].internshipID);
+            }
+            else{
+                for(let j = 0; j < filterVals.length; j++){
+                    if(internships[i].Industry == filterVals[j] || internships[i].School == filterVals[j] || internships[i].Major == filterVals[j]){
+                        validIds.push(internships[i].internshipID);
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(validIds);
+
+    // now that we finally have a set of valid ids, we can hide those that shouldn't be seen
+    const internshipCards = document.getElementsByClassName("internshipCard");
+    console.log(internshipCards);
+
+    // go through each internship, if it's valid, show it, otherwise display nothing
+    for(let i = 0; i < internshipCards.length; i++){
+        let toBeHidden = true;
+        for(let j = 0; j < validIds.length; j++){
+            if(validIds[j] == internshipCards[i].id){
+
+                console.log("Element won't be hidden");
+                toBeHidden = false;
+                break;
+            }
+        }
+        if(toBeHidden == true){
+            internshipCards[i].style.display = "none";
+        }
+        else{
+            internshipCards[i].style.display = "";
+        }
+    }
+});
